d7b97a6b52b5ee192f31c884c878207d9382a0b4
AlejandroM1ghty <alejandro@m1ghty.io>
2021-08-14 23:55:33 -0500

    Form 1:complete; Form 2:In process[DatePicker]

--- src/pages/dao-form/components/detail.vue
+++ src/pages/dao-form/components/detail.vue
@@ -82,17 +82,17 @@ div
       strong.
         Expiration Date (mm/dd/yyyy)
       p  {{calculateDate}}
-  div.container
-    q-input(
-      filled
-      v-model="form.delayedEffectiveDate"
-      mask="date"
-      :rules="['date']"
-    )
-      template(v-slot:apped)
-        q-icon(name="event" class="cursor-pointer")
-          q-popup-proxy(ref="qDateProxy" transition-show="scale" transition-hide="scale")
-            q-date(v-model='2019/02/01')
-              div(class="row items-center justify-end")
-                q-btn(v-close-popup label="Close" color="primary" flat)
+  //- div.container
+  //-   q-input(
+  //-     filled
+  //-     v-model="form.delayedEffectiveDate"
+  //-     mask="date"
+  //-     :rules="['date']"
+  //-   )
+  //-     template(v-slot:apped)
+  //-       q-icon(name="event" class="cursor-pointer")
+  //-         q-popup-proxy(ref="qDateProxy" transition-show="scale" transition-hide="scale")
+  //-           q-date(v-model='2019/02/01')
+  //-             div(class="row items-center justify-end")
+  //-               q-btn(v-close-popup label="Close" color="primary" flat)
 </template>
