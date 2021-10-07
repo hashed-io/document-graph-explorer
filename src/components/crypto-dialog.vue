eccb4adffd24e7033a93d43552fe76e738169de1
DaniPoot <danielpoot.uc2@gmail.com>
2021-10-07 10:00:36 -0500

    Added new component to manage secret key

---
+++ src/components/crypto-dialog.vue
@@ -0,0 +1,53 @@
+<template lang="pug">
+q-dialog(v-model="openDialog" persistent)
+  q-card
+    q-card-section
+      .text-h6 Enter key to encrypt and decrypt your data
+    q-form(
+        @submit="onSubmit"
+        class="q-gutter-md"
+    )
+      q-card-section
+        q-input(
+          v-model.trim="cryptoKey"
+          filled
+          label="Insert yout key"
+          :rules="[rules.required]"
+        )
+      q-card-actions(align="right")
+        q-btn(
+          color="primary"
+          :label="$t('common.buttons.confirm')"
+          type="submit"
+        )
+</template>
+
+<script>
+import { validation } from '~/mixins/validation'
+
+export default {
+  name: 'crypto-dialog',
+  mixins: [validation],
+  props: {
+    openDialog: {
+      required: true,
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      cryptoKey: undefined
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.$emit('close-dialog', this.cryptoKey)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
