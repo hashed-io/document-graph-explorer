216902a8a4a43aa347775bc77d63f9aa6d981a08
AlejandroM1ghty <alejandro@m1ghty.io>
2021-11-11 17:03:50 -0600

    Refactoring apollo services & actions

--- src/store/apollo/actions.js
+++ src/store/apollo/actions.js
@@ -1,3 +1,12 @@
+export const getType = async function ({ commit }, params) {
+  try {
+    const response = await this.$apolloApi.getType({ ...params })
+    return response
+  } catch (e) {
+    console.error('An error ocurred while trying to get documents', e)
+    throw new Error(e)
+  }
+}
 export const getDocuments = async function ({ commit }) {
   try {
     const response = await this.$apolloApi.getDocuments()
@@ -14,6 +23,16 @@ export const getAssignment = async function ({ commit }) {
     return response
   } catch (e) {
     console.error('An error ocurred while trying to get documents', e)
+    throw new Error(e)
+  }
+}
+
+export const getMembers = async function ({ commit }, params) {
+  try {
+    const response = await this.$apolloApi.getMembers({ ...params })
+    return response
+  } catch (e) {
+    console.error('An error ocurred while trying to get DHO by hash', e)
     throw new Error(e)
   }
 }
